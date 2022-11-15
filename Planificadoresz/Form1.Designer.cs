3ef709c5746cc863a190088add7affd94efb9aa7
RicardoRobleto97 <41806768+RicardoRobleto97@users.noreply.github.com>
2022-11-14 22:53:48 -0600

    Add project files.

---
+++ Planificadoresz/Form1.Designer.cs
@@ -0,0 +1,401 @@
+﻿namespace Planificadoresz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.PanelCPU = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PanelPrioridad = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CPUtxt = new System.Windows.Forms.TextBox();
+            this.Nametxt = new System.Windows.Forms.TextBox();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.algoritmosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.procesosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.porCPUToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.porMemoriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel1.SuspendLayout();
+            this.PanelCPU.SuspendLayout();
+            this.PanelPrioridad.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.LightCoral;
+            this.button1.FlatAppearance.BorderSize = 1000;
+            this.button1.Location = new System.Drawing.Point(12, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Prioridad";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button2.FlatAppearance.BorderSize = 10;
+            this.button2.Location = new System.Drawing.Point(12, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button3.FlatAppearance.BorderSize = 10;
+            this.button3.Location = new System.Drawing.Point(12, 173);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button4.FlatAppearance.BorderSize = 10;
+            this.button4.Location = new System.Drawing.Point(12, 215);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 36);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(146)))), ((int)(((byte)(72)))));
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(207, 623);
+            this.panel1.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.LightCoral;
+            this.button5.FlatAppearance.BorderSize = 1000;
+            this.button5.Location = new System.Drawing.Point(12, 257);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(154, 36);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button6.FlatAppearance.BorderSize = 10;
+            this.button6.Location = new System.Drawing.Point(12, 383);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(154, 36);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.AutoSize = true;
+            this.button7.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button7.FlatAppearance.BorderSize = 10;
+            this.button7.Location = new System.Drawing.Point(12, 299);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(154, 36);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.AutoSize = true;
+            this.button8.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button8.FlatAppearance.BorderSize = 10;
+            this.button8.Location = new System.Drawing.Point(12, 341);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(154, 36);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // PanelCPU
+            // 
+            this.PanelCPU.Controls.Add(this.label1);
+            this.PanelCPU.Location = new System.Drawing.Point(213, 28);
+            this.PanelCPU.Name = "PanelCPU";
+            this.PanelCPU.Size = new System.Drawing.Size(714, 498);
+            this.PanelCPU.TabIndex = 5;
+            this.PanelCPU.Visible = false;
+            this.PanelCPU.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelCPU_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Por CPU";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // PanelPrioridad
+            // 
+            this.PanelPrioridad.Controls.Add(this.textBox2);
+            this.PanelPrioridad.Controls.Add(this.textBox1);
+            this.PanelPrioridad.Controls.Add(this.dataGridView1);
+            this.PanelPrioridad.Controls.Add(this.label4);
+            this.PanelPrioridad.Controls.Add(this.label3);
+            this.PanelPrioridad.Controls.Add(this.CPUtxt);
+            this.PanelPrioridad.Controls.Add(this.Nametxt);
+            this.PanelPrioridad.Controls.Add(this.button9);
+            this.PanelPrioridad.Controls.Add(this.label2);
+            this.PanelPrioridad.Location = new System.Drawing.Point(211, 25);
+            this.PanelPrioridad.Name = "PanelPrioridad";
+            this.PanelPrioridad.Size = new System.Drawing.Size(714, 498);
+            this.PanelPrioridad.TabIndex = 6;
+            this.PanelPrioridad.Visible = false;
+            this.PanelPrioridad.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelPrioridad_Paint);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(280, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(33, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(226, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(33, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(43, 141);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(338, 149);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(175, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "#CPU";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Proceso";
+            // 
+            // CPUtxt
+            // 
+            this.CPUtxt.Location = new System.Drawing.Point(178, 68);
+            this.CPUtxt.Name = "CPUtxt";
+            this.CPUtxt.Size = new System.Drawing.Size(33, 22);
+            this.CPUtxt.TabIndex = 3;
+            // 
+            // Nametxt
+            // 
+            this.Nametxt.Location = new System.Drawing.Point(40, 67);
+            this.Nametxt.Name = "Nametxt";
+            this.Nametxt.Size = new System.Drawing.Size(98, 22);
+            this.Nametxt.TabIndex = 2;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(435, 57);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(122, 31);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "Agregar\'";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Por Prioridad";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.algoritmosToolStripMenuItem,
+            this.procesosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(937, 28);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // algoritmosToolStripMenuItem
+            // 
+            this.algoritmosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.porCPUToolStripMenuItem,
+            this.porMemoriaToolStripMenuItem});
+            this.algoritmosToolStripMenuItem.Name = "algoritmosToolStripMenuItem";
+            this.algoritmosToolStripMenuItem.Size = new System.Drawing.Size(97, 24);
+            this.algoritmosToolStripMenuItem.Text = "Algoritmos";
+            // 
+            // procesosToolStripMenuItem
+            // 
+            this.procesosToolStripMenuItem.Name = "procesosToolStripMenuItem";
+            this.procesosToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.procesosToolStripMenuItem.Text = "Procesos";
+            this.procesosToolStripMenuItem.Click += new System.EventHandler(this.procesosToolStripMenuItem_Click);
+            // 
+            // porCPUToolStripMenuItem
+            // 
+            this.porCPUToolStripMenuItem.Name = "porCPUToolStripMenuItem";
+            this.porCPUToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.porCPUToolStripMenuItem.Text = "Por CPU";
+            // 
+            // porMemoriaToolStripMenuItem
+            // 
+            this.porMemoriaToolStripMenuItem.Name = "porMemoriaToolStripMenuItem";
+            this.porMemoriaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.porMemoriaToolStripMenuItem.Text = "Por Memoria";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(937, 651);
+            this.Controls.Add(this.PanelPrioridad);
+            this.Controls.Add(this.PanelCPU);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.PanelCPU.ResumeLayout(false);
+            this.PanelCPU.PerformLayout();
+            this.PanelPrioridad.ResumeLayout(false);
+            this.PanelPrioridad.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Panel PanelCPU;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel PanelPrioridad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox CPUtxt;
+        private System.Windows.Forms.TextBox Nametxt;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem algoritmosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem procesosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem porCPUToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem porMemoriaToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ Planificadoresz/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Planificadoresz
+{
+    
+
+    public partial class Form1 : Form
+    {
+        Proceso con = new Proceso();
+       // public static List<Proceso> lista = new List<Proceso>();
+        public Form1()
+        {
+            InitializeComponent();
+            //. List<ProcesosCPU> lista_ProcesosCPU = new List<>();
+            //int [] valores;
+            
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PanelCPU.Visible = true;
+            PanelPrioridad.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PanelCPU_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PanelCPU.Visible = false;
+            PanelPrioridad.Visible = true;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            string nombreProceso = CPUtxt.Text.ToString();
+            int CPUvalue = Convert.ToInt32(CPUtxt.Text);
+             //public static List<Proceso> lista = new List<Proceso>();
+
+
+    }
+
+    private void PanelPrioridad_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void procesosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProcesosForm pf = new ProcesosForm();
+            pf.Visible = true;
+           
+        }
+    }
+}
